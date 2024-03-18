c54f8a03c5a6d97b00c69cbd0ab21f77e678a73d
behrad2k <behradsharifi8@gmail.com>
2024-03-18 10:52:33 +0330

    test

--- kernel.c
+++ kernel.c
@@ -5,5 +5,5 @@ void kernel_main(void)
 {
 	terminal_initialize();
 	puts("Hello, kernel World!\n test\n");
-    	printf("hi %s, test %l", "100", 100l);
+	printf("hi %s, test %d", "100", 100); // test
 }
--- libc/printf.c
+++ libc/printf.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int strlen(const char* str) 
-{
-	int len = 0;
-	while (str[len]) {
-		len++;
-	}
-	return len;
-}
 
 static char* itoa(int i) {
     if(i == 0) {
